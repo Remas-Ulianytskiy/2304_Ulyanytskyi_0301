aa468df62e9b75ea2ac68870b32369e858bb8159
Remas-Ulianytskiy <remas.omar.101@gmail.com>
2023-07-13 19:04:16 +0300

    Add project files.

---
+++ 2304_Ulyanytskyi_0301.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <deque>
+
+using namespace std;
+
+int main()
+{
+    const unsigned DEQUE_MAX_SIZE = 48;
+    const deque<int64_t> SEQUENCE = { -13, 0, 10, -4, 6, 11, 12, 28, 50, 89, 166, 304 };
+    deque<int64_t> sequence;
+
+    sequence = SEQUENCE;    
+
+    while (sequence.size() < DEQUE_MAX_SIZE)
+    {
+        int size = sequence.size();
+        int64_t preceding_element = sequence[2] - sequence[1] + sequence[0] + 1;
+        int64_t next_element = sequence[size-3] + sequence[size-2] + sequence[size-1] - 1;
+
+        sequence.push_back(next_element);
+        sequence.push_front(preceding_element);
+    }
+
+    cout << "Deque elements:";
+    for (int i = 0; i < sequence.size(); ++i)
+    {
+        if (i % 10 == 0)
+            cout << endl;
+        cout << sequence[i] << ' ';
+    }
+    cout << endl << "deque size: " << sequence.size() << endl;
+
+    return 0;
+}
